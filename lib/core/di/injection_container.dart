953d70289088be0531ecb7ccac64a75e5111301c
Francinildo Rodrigues <francis584@gmail.com>
2025-04-02 01:42:34 -0300

    add product_repo & cubit to DI

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,6 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import 'package:pos_machine/core/config/env_config.dart';
+import 'package:pos_machine/features/product/data/repositories/product_repository_impl.dart';
+import 'package:pos_machine/features/product/domain/repositories/product_repository.dart';
+import 'package:pos_machine/features/product/presentation/cubit/product_cubit.dart';
 import 'package:pos_machine/features/seller/data/repositories/seller_repository_impl.dart';
 import 'package:pos_machine/features/seller/domain/repositories/seller_repository.dart';
 import 'package:pos_machine/features/seller/presentation/cubit/seller_cubit.dart';
@@ -16,7 +20,11 @@ Future<void> init() async {
   getIt.registerLazySingleton<SellerRepository>(
     () => SellerRepositoryImpl(EnvConfig.apiUrl, getIt()),
   );
+  getIt.registerLazySingleton<ProductRepository>(
+    () => ProductRepositoryImpl(EnvConfig.apiUrl, getIt()),
+  );
 
   // Cubits
   getIt.registerFactory(() => SellerCubit(repository: getIt()));
+  getIt.registerFactory(() => ProductCubit(repository: getIt()));
 }
