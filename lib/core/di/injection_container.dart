09f0780f525cdac12c3ecb69b1792cff641bf647
Francinildo Rodrigues <francis584@gmail.com>
2025-04-02 13:47:42 -0300

    add sale repository & cubit to DI

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,10 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:pos_machine/core/config/env_config.dart';
+import 'package:pos_machine/core/services/cache_service.dart';
 import 'package:pos_machine/features/product/data/repositories/product_repository_impl.dart';
 import 'package:pos_machine/features/product/domain/repositories/product_repository.dart';
 import 'package:pos_machine/features/product/presentation/cubit/product_cubit.dart';
+import 'package:pos_machine/features/sale/data/repositories/sale_repository_impl.dart';
+import 'package:pos_machine/features/sale/domain/repositories/sale_repository.dart';
+import 'package:pos_machine/features/sale/presentation/cubit/sale_cubit.dart';
 import 'package:pos_machine/features/seller/data/repositories/seller_repository_impl.dart';
 import 'package:pos_machine/features/seller/domain/repositories/seller_repository.dart';
 import 'package:pos_machine/features/seller/presentation/cubit/seller_cubit.dart';
@@ -29,8 +34,12 @@ Future<void> init() async {
   getIt.registerLazySingleton<ProductRepository>(
     () => ProductRepositoryImpl(getIt(), getIt()),
   );
+  getIt.registerLazySingleton<SaleRepository>(
+    () => SaleRepositoryImpl(getIt()),
+  );
 
   // Cubits
   getIt.registerFactory(() => SellerCubit(repository: getIt()));
   getIt.registerFactory(() => ProductCubit(repository: getIt()));
+  getIt.registerFactory(() => SaleCubit(repository: getIt()));
 }
