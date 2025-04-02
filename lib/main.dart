328a6865bac8882050b1af4352735825e6b9ec93
Francinildo Rodrigues <francis584@gmail.com>
2025-04-02 18:25:03 -0300

    create multiblocprovider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:pos_machine/core/config/env_config.dart';
 import 'package:pos_machine/core/di/injection_container.dart' as di;
 import 'package:pos_machine/core/navigation/routes.dart';
 import 'package:pos_machine/core/theme/app_theme.dart';
+import 'package:pos_machine/features/product/service/cubit/product_cubit.dart';
+import 'package:pos_machine/features/sale/service/cubit/sale_cubit.dart';
+import 'package:pos_machine/features/seller/service/cubit/seller_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,19 +22,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          title: EnvConfig.appName,
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.lightTheme,
-          initialRoute: Routes.home, // Defina a rota inicial
-          onGenerateRoute: Routes.generateRoute,
-        );
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => di.getIt<SellerCubit>()),
+        BlocProvider(create: (context) => di.getIt<ProductCubit>()),
+        BlocProvider(create: (context) => di.getIt<SaleCubit>()),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            title: EnvConfig.appName,
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            initialRoute: Routes.home, // Defina a rota inicial
+            onGenerateRoute: Routes.generateRoute,
+          );
+        },
+      ),
     );
   }
 }
