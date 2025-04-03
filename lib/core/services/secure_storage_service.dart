1e00c4cec19de7bbc8cb6e69d83677185c05881a
Francinildo Rodrigues <francis584@gmail.com>
2025-04-03 15:05:59 -0300

    create secureStorageService

---
+++ lib/core/services/secure_storage_service.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'package:pos_machine/core/services/interfaces/secure_storage_interface.dart';
+
+class SecureStorageService implements SecureStorage {
+  final FlutterSecureStorage _storage;
+
+  SecureStorageService(this._storage);
+
+  @override
+  Future<void> saveToken({required String key, required String value}) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  @override
+  Future<String?> getToken({required String key}) async {
+    return await _storage.read(key: key);
+  }
+
+  @override
+  Future<void> deleteToken({required String key}) async {
+    await _storage.delete(key: key);
+  }
+
+  @override
+  Future<bool> hasToken({required String key}) async {
+    final token = await getToken(key: key);
+    return token != null && token.isNotEmpty;
+  }
+}
